1d99116cc9d2855dd1e1409e14ba5d806001e4e8
jantoy052003 <124265990+jantoy052003@users.noreply.github.com>
2023-03-12 21:57:52 +0800

    created booking page

---
+++ js/main.js
@@ -0,0 +1,14 @@
+document.getElementById("#book-now").addEventListener('click', book);
+function book() {
+    window.location.href="date-booking.html";
+}
+
+document.getElementById("take-me").addEventListener('click', takeMe);
+function takeMe() {
+    window.location.href="https://www.google.com/maps/place/Oceanside+Villas/@16.7043063,120.3360199,17z/data=!3m1!4b1!4m6!3m5!1s0x33918de56404fc0d:0xed8a04430aecc7db!8m2!3d16.7043012!4d120.3382086!16s%2Fg%2F11pzh05llg";
+}
+
+document.getElementById("book-btn").addEventListener('click', bookNow);
+function bookNow() {
+    window.location.href="https://www.airbnb.com/rooms/794422134501154265?adults=2&children=2&check_in=2023-03-17&check_out=2023-03-19&federated_search_id=e1079ee8-cdab-4323-a7e1-9b8d26c82a36&source_impression_id=p3_1677148542_V13GOIsBymUitAOv";
+}
